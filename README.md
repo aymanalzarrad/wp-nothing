f9e9132ab43dc4ea12112796aa15b9d72bbc0fcf
Ayman Al Zarrad <aymanalzarrad@gmail.com>
2016-03-07 20:56:27 +0100

    Repo up

---
+++ README.md
@@ -0,0 +1,17 @@
+WP Nothing
+==========
+The first and only WordPress plugin that does absolutely nothing.
+
+Getting started
+---------------
+1. Download or clone the repo.
+2. upload the plugin to your plugins directory.
+3. Profit.
+
+Feature request
+---------------
+Let me know if you have a nothing feature that you would to see it in the plugin.
+
+License
+-------
+GPL
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * WP Nothing
+ *
+ * @package     WP_Nothing
+ * @author      Ayman
+ * @copyright   2016 Ayman
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: WP Nothing
+ * Description: The first and only WordPress plugin that does absolutely nothing.
+ * Version:     1.0.0
+ * Author:      Ayman
+ * Text Domain: wp-nothing
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
